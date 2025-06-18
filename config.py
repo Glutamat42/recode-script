b15a04eee0dd03865486a92728209e137de7ea0f
Markus <markus.heck13396@gmx.de>
2025-06-18 22:43:21 +0200

    update defaults

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ class Config:
         # Audio encoding: stereo/mono uses libopus with bitrate, multi-channel uses libfdk_aac with VBR
         self.audio_bitrate_stereo = 160
         self.audio_bitrate_threshold = 1.25
-        self.audio_multichannel_vbr_level = 3  # used for streams witht more than 2 channels. VBR level for libfdk_aac multi-channel (1-5, where 1=highest quality, 5=lowest)
+        self.audio_multichannel_vbr_level = 2  # used for streams witht more than 2 channels. VBR level for libfdk_aac multi-channel (1-5, where 1=highest quality, 5=lowest)
         self.keyint_seconds = "-2"
-        self.max_parallel_encodes = 1
+        self.max_parallel_encodes = 2
         self.video_exts = {".mp4", ".mkv", ".mov", ".webm", ".avi"}
         self.compressed_suffix = "_compressed"
         self.crop_timestamps = [90, 180, 300]
